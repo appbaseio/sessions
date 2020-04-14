b889f441b92107602d61caaab07115339360e3d8
lakhansamani <lakhan.m.samani@gmail.com>
2020-04-14 18:38:21 +0530

    add same site to cookie.go

--- cookie.go
+++ cookie.go
@@ -14,6 +14,7 @@ func newCookieFromOptions(name, value string, options *Options) *http.Cookie {
 		MaxAge:   options.MaxAge,
 		Secure:   options.Secure,
 		HttpOnly: options.HttpOnly,
+		SameSite: options.SameSite,
 	}
 
 }
